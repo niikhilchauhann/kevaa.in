086182fad183d3ff37fcb96596d5c1e94c5bf2e4
vansh-justcharge <vansh.sharma@justchargetechnologies.com>
2025-07-29 16:39:17 +0530

    Footer Cta Changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,13 +13,9 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="newsletter">
-                <h2>STAY UP TO DATE ABOUT<br />OUR LATEST OFFERS</h2>
+                <h2>WANT TO COLLAB WITH US<br />JOIN KEVAA NOW..!!</h2>
                 <div className="newsletter-form">
-                    <div className="input-box">
-                        <MdOutlineEmail />
-                        <input type="email" placeholder="Enter your email address" />
-                    </div>
-                    <button>Subscribe to Newsletter</button>
+                    <button>Join Now</button>
                 </div>
             </div>
 
@@ -32,7 +28,6 @@ export default function Footer() {
                             <FaTwitter />
                             <FaFacebookF />
                             <FaInstagram />
-                            <FaGithub />
                         </div>
                     </div>
                 </div>
